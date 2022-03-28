92a469afdca17d9096f0b9d1df6fbb372284439c
Andrii Ostapenko <droid85@yahoo.com>
2022-03-28 18:33:08 +0300

    created even_or_odd method

--- test_skeleton.rb
+++ test_skeleton.rb
@@ -10,7 +10,11 @@ class TestSkeleton
   # TestSkeleton.new.even_or_odd(0) should return "even"
   # TestSkeleton.new.even_or_odd(-42) should return "even"
   def even_or_odd(number)
-    # Your solution should be here
+    if number % 2 == 0
+      puts "even"
+    else
+      puts "odd"
+    end
   end
 
   # https://www.codewars.com/kata/5583090cbe83f4fd8c000051
